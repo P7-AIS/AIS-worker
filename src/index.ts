8de537235d09760838b9b8922e547fdcd9097b7a
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2024-11-05 11:16:48 +0100

    jes

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Queue } from 'bullmq'
 import dotenv from 'dotenv'
 import AISWorker from './implementations/AISWorker'
 import RandomScorer from './implementations/RandomScorer'
-import SimpleScorer from './implementations/SimpleScorer'
 import HashScorer from './implementations/HashScorer'
+import VesselScore from './implementations/VesselScore'
 
 dotenv.config()
 
@@ -17,7 +17,7 @@ const connection = { host: REDIS_IP, port: Number(REDIS_PORT) }
 const queue = new Queue(REDIS_QUEUE_NAME, { connection })
 
 const randomScorer = new RandomScorer()
-const simpleScorer = new SimpleScorer()
+const simpleScorer = new VesselScore()
 const hashScorer = new HashScorer()
 
 const aisWorker = new AISWorker(queue, connection, randomScorer, simpleScorer, hashScorer)
